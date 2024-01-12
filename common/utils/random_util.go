1e4dd8c7ea4c18b8a7671d7f9fbea80c317d1c09
史良一 <568299006@qq.com>
2024-01-12 19:42:39 +0800

    bug修复

--- common/utils/random_util.go
+++ common/utils/random_util.go
@@ -3,13 +3,22 @@ package utils
 import "math/rand"
 
 func RandomInt64(min, max int64) int64 {
+	if (max - min) <= 0 {
+		return min
+	}
 	return rand.Int63n(max-min) + min
 }
 
 func RandomInt32(min, max int32) int32 {
+	if (max - min) <= 0 {
+		return min
+	}
 	return rand.Int31n(max-min) + min
 }
 
 func RandomInt(min, max int) int {
+	if (max - min) <= 0 {
+		return min
+	}
 	return rand.Intn(max-min) + min
 }
