1fbd789166aa8e1c694b359d4c6d15e061fd5a99
史良一 <568299006@qq.com>
2024-01-13 11:55:33 +0800

    测试client添加默认处理器

--- game_client/client_http_server.go
+++ game_client/client_http_server.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"github.com/ambitiousmice/go-one/common/json"
 	"github.com/ambitiousmice/go-one/common/log"
+	"github.com/ambitiousmice/go-one/game/common"
 	"github.com/golang/protobuf/proto"
 	"net/http"
 	"reflect"
@@ -46,6 +47,10 @@ func timeoutMiddleware() gin.HandlerFunc {
 
 var cmdParamContext = make(map[uint16]reflect.Type)
 
+var cmdParamNames = make(map[uint16]string)
+
+var respParamContext = make(map[uint16]reflect.Type)
+
 func RegisterCmdParam(cmd uint16, param any) {
 	objVal := reflect.ValueOf(param)
 	paramType := objVal.Type()
@@ -54,6 +59,61 @@ func RegisterCmdParam(cmd uint16, param any) {
 		paramType = paramType.Elem()
 	}
 	cmdParamContext[cmd] = paramType
+}
+
+func RegisterCmdParamAndResp(cmdName string, cmd uint16, param any, resp any) {
+
+	if cmdName != "" {
+		cmdParamNames[cmd] = cmdName
+	}
+
+	if param != nil {
+		objVal := reflect.ValueOf(param)
+		paramType := objVal.Type()
+
+		if paramType.Kind() == reflect.Ptr {
+			paramType = paramType.Elem()
+		}
+		cmdParamContext[cmd] = paramType
+	}
+
+	if resp != nil {
+		objVal := reflect.ValueOf(resp)
+		paramType := objVal.Type()
+
+		if paramType.Kind() == reflect.Ptr {
+			paramType = paramType.Elem()
+		}
+		respParamContext[cmd] = paramType
+	}
+
+}
+
+func DefaultProcessor(client *Client, Cmd uint16, param []byte) {
+	log.Infof("%s: serviceCmd:%s", cmdParamNames[Cmd], Cmd)
+
+	if respParamContext[Cmd] == nil {
+		if param == nil || len(param) == 0 {
+			log.Infof("%s: serviceCmd:%s，未返回数据", cmdParamNames[Cmd], Cmd)
+			return
+		}
+		if param != nil && len(param) > 0 {
+			log.Infof("未注册返回值类型，serviceCmd:%s，未返回数据长度：%s ,resp: ", Cmd, len(param), string(param))
+			return
+		}
+	}
+
+	if respParamContext[Cmd] != nil {
+		respData := reflect.New(respParamContext[Cmd]).Interface().(proto.Message)
+
+		err := common.UnPackMsg(param, respData)
+		if err != nil {
+			log.Errorf("%s:serviceCmd：%s unpack msg error: %s", cmdParamNames[Cmd], Cmd, err.Error())
+			return
+		}
+		s, _ := json.MarshalToString(respData)
+		log.Infof("%s: serviceCmd:%s,返回数据:%s", cmdParamNames[Cmd], Cmd, s)
+	}
 }
 
 type TestCmdReq struct {
@@ -75,12 +135,16 @@ func httpHandler(c *gin.Context) {
 
 		dataBytes, err := json.Marshal(req.Data)
 
+		s, _ := json.MarshalToString(req.Data)
+		log.Infof("cmd: %s ,请求参数:%s", req.Cmd, s)
+
 		err = json.Unmarshal(dataBytes, &reqData)
 
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
+
 		ClientContext[req.PID].SendGameData(req.Cmd, reqData)
 	} else {
 		ClientContext[req.PID].SendGameData(req.Cmd, nil)
