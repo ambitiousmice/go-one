ee5d7e5dd4d5c79d3d76fba5f5cc9add4b03fc95
史良一 <568299006@qq.com>
2024-05-29 19:33:47 +0800

    代码提交

--- common/utils/string_util.go
+++ common/utils/string_util.go
@@ -233,3 +233,12 @@ func ByteArrayToJsonStruct(b []byte, i interface{}) (interface{}, error) {
 	}
 	return i, nil
 }
+
+func Contains[T comparable](slice []T, value T) bool {
+	for _, v := range slice {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
